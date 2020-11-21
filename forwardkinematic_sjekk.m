cc1c7d780b248440b7b6213097812af47f658e20
celinepro <71331307+celinepro@users.noreply.github.com>
2020-11-21 21:56:03 +0100

    Update and rename forwardkinematic.m to forwardkinematic_sjekk.m

--- forwardkinematic_sjekk.m
+++ forwardkinematic_sjekk.m
@@ -1,12 +1,6 @@
+%Sjekker om den utregnede forward kinematics stemmer overens med det toolboxen regner ut
 clear all
-% import ETS3.*
-links = [
-	Revolute('d', 30, 'alpha', -pi/2)
-    Revolute('a', -100, 'alpha', pi)
-	Revolute('a', -100)
-	Revolute('a', -100)
-	%Revolute('a', -100)
-	];
+import ETS3.*
 syms q1 q2 q3 q4
 L(1) = Link([q1 30 0 -pi/2 0]);
 L(2) = Link([q2 0 -100 pi 0]);
@@ -18,4 +12,4 @@ five_link = SerialLink(L, 'name', 'forklift');
 
 TE = five_link.fkine([q1 q2 q3 q4])
 
-%TE = five_link.fkine([pi/3 pi/3 pi/3 pi/3])
+TE = five_link.fkine([0 0 0 0])
